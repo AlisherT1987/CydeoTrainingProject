084e97b36e8d2b57a0c954264d5edebaad963def
Alisherb28 <alish.tussupbaev@gmail.com>
2023-04-10 18:20:56 -1000

    create new project

---
+++ src/test/java/com/school/utilities/Environment.java
@@ -0,0 +1,53 @@
+package com.school.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Environment {
+
+        public static final String URL;
+        public static final String BASE_URL;
+        public static final String DB_USERNAME;
+        public static final String DB_PASSWORD;
+        public static final String DB_URL;
+        public static final String USERNAME;
+        public static final String USER_PASSWORD;
+
+
+    static{
+        Properties properties = null;
+        String environment = System.getProperty("environment") != null ? environment = System.getProperty("environment") : ConfigurationReader.getProperty("environment");
+        //this field will get its value from configuration.properties file environment key /qa1 qa2 qa3
+        //String environment = ConfigurationReader.getProperty("environment");
+
+
+
+        try {
+            //where is our file ?, path is holding that one
+            String path = System.getProperty("user.dir") + "/src/test/resources/environments/" + environment + ".properties";
+
+            FileInputStream input = new FileInputStream(path);
+            properties = new Properties();
+            properties.load(input);
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        URL = properties.getProperty("UI_URL");
+        BASE_URL = properties.getProperty("base_URL");
+        DB_USERNAME = properties.getProperty("dbUsername");
+        DB_PASSWORD = properties.getProperty("dbPassword");
+        DB_URL = properties.getProperty("dbURL");
+        USERNAME = properties.getProperty("UI_Username");
+        USER_PASSWORD = properties.getProperty("UI_password");
+
+
+
+
+
+    }
+
+
+}
